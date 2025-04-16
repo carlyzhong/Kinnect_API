7bcb8e49a9a9b831c61ab9129bedea693bbdd894
carlyzhong <zhongyjcarly@gmail.com>
2025-04-16 13:04:14 +0100

    tested seeding with some simple queries.

--- db/queries-dbs.sql
+++ db/queries-dbs.sql
@@ -1,3 +1,30 @@
 \c nc_news
 
-\echo 'Get all of the users'
+\echo '\n Get all of the users: \n'
+SELECT * FROM users;
+
+\echo '\n Get all of the articles where the topic is coding: \n'
+SELECT * FROM articles
+WHERE topic = 'coding';
+
+\echo '\n Get all of the comments where the votes are less than zero: \n'
+SELECT * FROM comments
+WHERE votes < 0
+ORDER BY votes ASC;
+
+\echo '\n Get all of the topics: \n'
+SELECT * FROM topics;
+
+\echo '\n Get all of the articles by user grumpy19 \n'
+SELECT * FROM articles
+WHERE author = 'grumpy19';
+
+\echo '\n Get all of the comments that have more than 10 votes\n'
+SELECT * FROM comments
+WHERE votes >= 10
+ORDER BY votes DESC;
+
+
+
+
+
