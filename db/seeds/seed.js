796e7e810a04a13f622d43bd876d519a5da6c568
carlyzhong <zhongyjcarly@gmail.com>
2025-04-23 18:19:38 +0100

    small update

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -41,10 +41,8 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
         return db.query(`CREATE TABLE articles(
   article_id SERIAL PRIMARY KEY,
   title VARCHAR(500) NOT NULL,
-  topic VARCHAR(200),
-  FOREIGN KEY (topic) REFERENCES topics(slug),
-  author VARCHAR(50),
-  FOREIGN KEY (author) REFERENCES users(username),
+  topic VARCHAR(200) REFERENCES topics(slug),
+  author VARCHAR(50) REFERENCES users(username),
   body TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   votes INT DEFAULT 0,
@@ -53,12 +51,10 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
       .then(() => {
         return db.query(`CREATE TABLE comments(
   comment_id SERIAL PRIMARY KEY,
-  article_id INT,
-  FOREIGN KEY (article_id) REFERENCES articles(article_id),
+  article_id INT REFERENCES articles(article_id),
   body TEXT,
   votes INT DEFAULT 0,
-  author VARCHAR(50),
-  FOREIGN KEY (author) REFERENCES users(username),
+  author VARCHAR(50) REFERENCES users(username),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`);
       })
 
