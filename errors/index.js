c3c32b54e42fc305452a158a692d4ae8613345bf
carlyzhong <zhongyjcarly@gmail.com>
2025-04-29 12:13:21 +0100

    added endpoint with testings: 3-CORE: GET /api/articles/:article_id

--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,24 @@
 exports.handleEndpointError = (req, res) => {
-  res.status(404).send({ msg: "Nothing to see here!" });
+  res.status(404).send({ msg: "Invalid Endpoint!" });
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send(err);
+  } else {
+    next(err);
+  }
+};
+
+exports.handleSQLErrors = (err, req, res, next) => {
+  // console.log(err, "inside sql error handling!!!!!!!");
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid Input!" });
+  } else {
+    next(err);
+  }
+};
+
+exports.handleServerErrors = (err, req, res, next) => {
+  res.status(500).send({ msg: "Internal Server Error!!" });
 };
