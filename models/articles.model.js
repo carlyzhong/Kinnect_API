e37a6993b1a9ee72869f3787db7fee4cf2116045
carlyzhong <zhongyjcarly@gmail.com>
2025-04-29 14:22:46 +0100

    4-GET /api/articles

--- models/articles.model.js
+++ models/articles.model.js
@@ -14,3 +14,17 @@ exports.selectArticleById = (id) => {
       return article;
     });
 };
+
+exports.selectArticles = () => {
+  return db
+    .query(
+      `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, count(comments.comment_id)::INT AS comment_count
+        FROM articles
+        LEFT JOIN comments ON comments.article_id = articles.article_id
+        GROUP BY articles.article_id
+        ORDER BY articles.created_at DESC;`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
