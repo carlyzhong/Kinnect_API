1737424a7e457bc2c91ccd20f10ce1257850bc21
carlyzhong <zhongyjcarly@gmail.com>
2025-06-22 21:59:00 +0100

    added tag data for testing

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,6 +1,6 @@
 exports.articleData = require("./articles.js");
 exports.commentData = require("./comments.js");
-exports.topicData = require("./topics.js");
+exports.topicData = require("./tags.js");
 exports.userData = require("./users.js");
 
 // console.log(this.userData);
---
+++ db/data/test-data/tags.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    tag_name: "tag_name_1",
+  },
+  {
+    tag_name: "tag_name_2",
+  },
+  {
+    tag_name: "tag_name_1",
+  },
+  {
+    tag_name: "tag_name_2",
+  },
+];
